b5e6154e048dc76bf756ed3eacee057459569ded
o1123516766542617 <o1123516766542617@outlook.com>
2019-10-17 18:54:52 -0400

    write `Person` class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,3 +7,16 @@ function heading (content) {
 }
 
 /**************************************/
+
+class Person {
+  constructor (attrs) {
+    this.name     = attrs.name;
+    this.age      = attrs.age;
+    this.location = attrs.location;
+  }
+  speak () {
+    console.log (
+      `Hello, my name is ${this.name}. I am from ${this.location}.`
+    );
+  }
+}
