4894535f649b02736617ddcafce2a194ccc39a0e
o1123516766542617 <o1123516766542617@outlook.com>
2019-10-17 19:02:13 -0400

    write `Instructor` class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -27,7 +27,23 @@ class Person {
 
 /*--- Instructor ---*/
 
-
+class Instructor extends Person {
+  constructor (attrs) {
+    this.specialty   = attrs.specialty;
+    this.favLanguage = attrs.favLanguage;
+    this.catchPhrase = attrs.catchPhrase;
+  }
+  demo (subject) {
+    console.log (
+      `Today we are learning about ${subject}.`
+    );
+  }
+  grade (student , subject) {
+    console.log (
+      `${student.name} receives a perfect score on ${subject}!`
+    )
+  }
+}
 
 /*--- Student ---*/
 
